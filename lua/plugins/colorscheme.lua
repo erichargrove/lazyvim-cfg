93df279a1052e82e01dbfe1b425adbe89f4f9ce4
Eric Hargrove <hargrove@mail.gvsu.edu>
2024-09-25 08:41:23 -0400

    Adding colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  { "navarasu/onedark.nvim", name = "onedark" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
