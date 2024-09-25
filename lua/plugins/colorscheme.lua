676e5182291138b9d838929eee7722f9ea51acb4
Eric Hargrove <hargrove@mail.gvsu.edu>
2024-09-25 09:07:03 -0400

    Configuring colors, adding mason stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   { "navarasu/onedark.nvim", name = "onedark" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin",
     },
   },
 }
