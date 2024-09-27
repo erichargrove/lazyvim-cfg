c9517fe2c363d3fff195d4430f30cfe4004a477b
Eric Hargrove <hargrove@mail.gvsu.edu>
2024-09-27 17:26:24 -0400

    formatting for javascript

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         ["html"] = { "prettier" },
+        ["javascript"] = { "prettier" },
         ["python"] = { "yapf" },
         ["C"] = { "clang-format" },
       },
